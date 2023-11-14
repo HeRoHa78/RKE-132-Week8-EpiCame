45a302e6b9143fd572f13ebb9fca8821b0fb723a
Helvi <helviroman@gmail.com>
2023-11-14 20:59:41 +0200

    add week8 code

---
+++ Week8HomeworkChuck/Program.cs
@@ -0,0 +1,17 @@
+﻿string path = @"C:\Users\Lenovo\Documents\data";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] fileContentArray)
+{
+    int i= 1; 
+    foreach (string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {line}");
+        i++;
+    }
+}
+
